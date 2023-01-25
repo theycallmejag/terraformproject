e911c48f8f51e8db108504c073e18e8db8065dfb
Michael Jagiello <michaeljagiello@Michaels-MBP.fios-router.home>
2023-01-24 22:37:42 -0500

    corrected line

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
    backend "s3" {
      encrypt = true  
-     bucket = "jags-levlup-terraform-state2"
+     bucket = "jags-levlup-terraform-state"
      dynamodb_table = "terraform-state-lock-dynamo"
      key    = "terraform.tfstate"
      region = "us-east-2"
