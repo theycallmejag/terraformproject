1187465e8f05c032f14b6e0edd41264e77cc8688
Michael Jagiello <michaeljagiello@Michaels-MBP.fios-router.home>
2023-02-02 20:59:22 -0500

    revised

--- S3.tf
+++ S3.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket" "jags-terraform-bucket-v1" {
+resource "aws_s3_bucket" "jags-terraform-bucket-v2" {
 
-     bucket = "jags-terraform-bucket-v1"
+     bucket = "jags-terraform-bucket-v2"
      lifecycle {
          prevent_destroy = true
      }
--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
    backend "s3" {
      encrypt = true  
-     bucket = "jags-terraform-bucket-v1"
+     bucket = "jags-terraform-bucket-v2"
      dynamodb_table = "terraform-state-lock-dynamo"
      key    = "terraform.tfstate"
      region = "us-east-2"
