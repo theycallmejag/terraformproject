524b30b173317be7f04f24ea082eb12a2dba052d
Michael Jagiello <michaeljagiello@Michaels-MBP.fios-router.home>
2023-02-08 18:22:44 -0500

    revised lifecycle

--- S3.tf
+++ S3.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "jags-terraform-bucket-v2" {
 
      bucket = "jags-terraform-bucket-v2"
      lifecycle {
-         prevent_destroy = true
+         prevent_destroy = false
      }
  }
